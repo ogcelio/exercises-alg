d28c9c13c6a7e4673ebe50ba2a46b28be9bbad85
ogcelio <joaocelioaraujosander@gmail.com>
2025-09-05 10:39:16 -0300

    adicionando esqueleto lista encadeada

---
+++ linked_list.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+struct linked_list {
+	char *string;
+	struct linked_list *next;
+};
+
+typedef struct linked_list item;
+
+void
+remove_item(item* initial, int index)
+{
+	
+}
+
+void
+add_item(item* initial, int index)
+{
+
+}
+
+void
+print_list(item* initial)
+{
+
+}
+
+int
+main()
+{
+	return 0;
+}
